1690784832867f46b6258f3a864910b9576e367e
Nuke928 <nuke928@gmail.com>
2014-01-04 03:20:05 +0100

    Implemented a bit of LuaMain
    Also added lua5.1 to the project

--- Ponykart++/Lua/LuaMain.h
+++ Ponykart++/Lua/LuaMain.h
@@ -1,8 +1,12 @@
 #ifndef LUAMAIN_H_INCLUDED
 #define LUAMAIN_H_INCLUDED
 
+#include <exception>
 #include <vector>
 #include <functional>
+#include <mutex>
+
+struct lua_State;
 
 namespace Ponykart
 {
@@ -14,11 +18,30 @@ namespace Lua
 
 using LuaEvent = std::vector<std::function<void ()>>;
 
-class LuaMain // TODO: Implement LuaMain
+class LuaMain
 {
 public:
-	LuaMain();
+	explicit LuaMain();
+
+	void registerLuaFunctions(std::function<void ()> func);
+	void runRegisterEvent();
+
 	void doFunctionForLThing(std::string& functionName, Actors::LThing* thing);
+
+    void doFile(const std::string &filename);
+    void doString(const std::string &s);
+
+    void quit();
+    void restart();
+
+    void print(const std::string &s);
+private:
+    lua_State *lua;
+    std::mutex luaMutex;
+
+    LuaEvent onRegister;
+
+    void initLuaState();
 };
 
 } // Lua
