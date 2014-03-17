d87a388c91f24e5233efcee5b6c9d67ac3314349
Tux3 / Mlkj / !Lev.uXFMLA <barrdetwix@gmail.com>
2014-03-17 15:51:27 +0100

    Throw "Not implemented" in WiimotePlayer ctor
    We'd need Wiimotelib and I'm not implementing this right now either.
    Code will still NOT link.

--- Ponykart++/Players/WiimotePlayer.cpp
+++ Ponykart++/Players/WiimotePlayer.cpp
@@ -1,2 +1,11 @@
 #include "pch.h"
 #include "Players/WiimotePlayer.h"
+
+using namespace Ponykart::Levels;
+using namespace Ponykart::Players;
+
+WiimotePlayer::WiimotePlayer(LevelChangedEventArgs* eventArgs, int id)
+: Player(eventArgs, id, false)
+{
+	throw std::string("WiimotePlayer::WiimotePlayer: Not implemented ! Need wiimotelib first");
+}
