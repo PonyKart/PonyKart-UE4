b697307325f48c129e3e5d70c7b05fdbb5ed7a0b
Tux3 / Mlkj / !Lev.uXFMLA <barrdetwix@gmail.com>
2014-03-17 15:30:08 +0100

    Throw "Not implemented" in GameUIManager.
    We need to link with GameUIManager::setItemLevel, called from
    HumanPlayer's ctor, called from PlayerManager's onLevelLoad.
    But this depends directly on the GUI system, and it's not even designed
    yet.
    And I'm not implementing the whole GUI system just because I said I was
    implementing PlayerManager and its dependencies.
    Have some unconditional exception throwing in the meantime.

--- Ponykart++/UI/GameUIManager.cpp
+++ Ponykart++/UI/GameUIManager.cpp
@@ -3,3 +3,17 @@
 
 using namespace Ponykart::UI;
 
+void GameUIManager::setItemLevel(int level)
+{
+	/// TODO: Implement setItemLevel once we've got the CEGUI design part done
+	/// > IMPLYING WINDOWS FORMS
+	//if (level <= 0)
+	//	itembox.Bitmap = new Bitmap("media/gui/items/no item box.png");
+	//else if (level == 1)
+	//	itembox.Bitmap = new Bitmap("media/gui/items/lv1 box.png");
+	//else if (level == 2)
+	//	itembox.Bitmap = new Bitmap("media/gui/items/lv2 box.png");
+	//else if (level >= 3)
+	//	itembox.Bitmap = new Bitmap("media/gui/items/lv3 box.png");
+	throw std::string("[CRITICAL][NOTIMPLEMENTED][DARKSNYDERPLSFIX]GameUIManager::setItemLevel: Not implemented.");
+}
